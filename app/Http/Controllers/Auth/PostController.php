006e8de3bab1592ea35fde83e0f6dc97d7c574a9
Peds Yorpo <pedroyorpo17@gmail.com>
2025-03-29 22:28:06 +0800

    push

--- app/Http/Controllers/Auth/PostController.php
+++ app/Http/Controllers/Auth/PostController.php
@@ -18,7 +18,6 @@ class PostController extends Controller
      */
     public function index(Request $request)
     {
-        //
             if(Auth::check()){
                 try {
                     $requestedCode = $request->code;
