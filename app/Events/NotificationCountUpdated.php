b3cc1ab2b7cac021ceeab6c81a55db91c8e68ab2
Peds Yorpo <pedroyorpo17@gmail.com>
2025-04-05 13:48:59 +0800

    commit

--- app/Events/NotificationCountUpdated.php
+++ app/Events/NotificationCountUpdated.php
@@ -1,22 +1,28 @@
 <?php
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class NotificationCountUpdated implements ShouldBroadcast
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationCountUpdated
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $userId;
     public $unreadCount;
 
-    public function __construct($unreadCount)
+    public function __construct($userId, $unreadCount)
     {
+        $this->userId = $userId;
         $this->unreadCount = $unreadCount;
     }
 
     public function broadcastOn()
     {
-        return new PrivateChannel('user.' . auth()->id());
-    }
-    
-    public function broadcastAs()
-    {
-        return 'NotificationCountUpdated';
+        return new PrivateChannel('user.' . $this->userId);
     }
 }
