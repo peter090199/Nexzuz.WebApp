58466f708f1c13b3bcace6cf01220651b2fe74a2
Peds Yorpo <pedroyorpo17@gmail.com>
2025-04-05 12:44:54 +0800

    commit

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,10 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
 Broadcast::channel('chat.{receiver_id}', function ($user, $receiver_id) {
     return (int) $user->id === (int) $receiver_id;
 });
