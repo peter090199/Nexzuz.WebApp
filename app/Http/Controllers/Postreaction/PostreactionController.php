a61c28046778b4fed62164f84023105a951f4abb
Rein1115 <142583344+Rein1115@users.noreply.github.com>
2025-05-16 23:30:05 +0800

    update

--- app/Http/Controllers/Postreaction/PostreactionController.php
+++ app/Http/Controllers/Postreaction/PostreactionController.php
@@ -105,7 +105,7 @@ class PostreactionController extends Controller
     {
         //
         if (Auth::check()) {
-            $data = DB::select('CALL sprocReactionSave(?, ?, ?)', [
+            $data = DB::select('EXECUTE sprocReactionSave(?, ?, ?)', [
                 Auth::user()->code,
                 $id,
                 $request->reaction 
