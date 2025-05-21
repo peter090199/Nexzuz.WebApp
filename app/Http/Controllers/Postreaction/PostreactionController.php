7efbe40119444289f61c569c5e0cf7ec57621477
rein1115 <reinjunelaride@gmail.com>
2025-05-21 09:11:14 +0800

    commit

--- app/Http/Controllers/Postreaction/PostreactionController.php
+++ app/Http/Controllers/Postreaction/PostreactionController.php
@@ -93,9 +93,10 @@ class PostreactionController extends Controller
     {
 
 
-        $data = DB::select('SELECT code,SELECT getFullname(711) AS fullname,SELECT getUserprofilepic(code) AS photo_pic,post_uuidOrUind,reaction,created_at FROM reactions WHERE post_uuidOrUind', [$id]);
+        $data = DB::select('SELECT code,getFullname(code) AS fullname,getUserprofilepic(code) AS photo_pic,
+            post_uuidOrUind,reaction,created_at FROM reactions WHERE post_uuidOrUind = ?', [$id]);
 
-        return $data;
+            return response()->json($data);
 
         // $data = DB::select('SELECT code,SELECT getFullname(711) AS fullname,getUserprofilepic(code) AS photo_pic,post_uuidOrUind,reaction,created_at FROM reactions WHERE post_uuidOrUind', [$id]);
 
