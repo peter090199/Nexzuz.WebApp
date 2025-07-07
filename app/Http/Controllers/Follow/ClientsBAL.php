0ed707ffb9933c08df3b15a4aa0f9dcf0fee8289
Peds Yorpo <pedroyorpo17@gmail.com>
2025-07-07 23:19:18 +0800

    COMMIT

--- app/Http/Controllers/Follow/ClientsBAL.php
+++ app/Http/Controllers/Follow/ClientsBAL.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Follow;
+namespace App\Business;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use DB;
-use App\DataAccess\DAL\ClientsDAL;
-class ClientsBAL extends Controller
+use App\DataAccess\ClientsDAL;
+
+class ClientsBAL
 {
     protected $resourceDAL;
 
@@ -17,7 +15,6 @@ class ClientsBAL extends Controller
 
     public function getListClients()
     {
-       return $this->resourceDAL->getListClients();
+        return $this->resourceDAL->getListClients();
     }
-
 }
--- app/Models/DAL/ClientsDAL.php
+++ app/Models/DAL/ClientsDAL.php
@@ -2,57 +2,14 @@
 
 namespace App\DataAccess;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\DB;
 
-class Resource extends Model
+class ClientsDAL
 {
-    use HasApiTokens, HasFactory, Notifiable;
-    
-    protected $fillable = [
-        'code',
-        'fname',
-        'lname',
-        'mname',
-        'fullname',
-        'contact_no',
-        'age',
-        'email',
-        'profession',
-        'company',
-        'industry',
-        'companywebsite',
-        'role_code',
-        'designation',
-        'h1_fname',
-        'h1_lname',
-        'h1_mname',
-        'h1_fullname',
-        'h1_contact_no',
-        'h1_email',
-        'h1_address1',
-        'h1_address2',
-        'h1_city',
-        'h1_province',
-        'h1_postal_code',
-        'h1_companycode',
-        'h1_rolecode',
-        'h1_designation',
-        'created_by',
-        'updated_by'
-    ];
-
-
-     public function getListClients()
+    public function getListClients()
     {
         return DB::table('resources')
             ->select('fullname', 'profession', 'role_code')
             ->get();
     }
-
-
-
 }
