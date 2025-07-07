61177bc346d6dbc9ea20e4c28aa192b6d231a7b1
Peds Yorpo <pedroyorpo17@gmail.com>
2025-07-07 23:32:55 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -143,6 +143,8 @@ Route::middleware(['auth:sanctum','checkstatus'])->group(function () {
 
     //List clients base on rrofile
     Route::get('getListClients', [ClientsBAL::class, 'getListClients']);
+    Route::get('getListClients', [Clients::class, 'getListClients']);
 
+ 
 
 });
