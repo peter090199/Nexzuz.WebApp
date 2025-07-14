ff50fb38de9fa5647b64391f10ad067cafc8cc24
Peds Yorpo <pedroyorpo17@gmail.com>
2025-07-14 21:27:32 +0800

    COMMIT

--- app/Http/Controllers/Follow/FollowController.php
+++ app/Http/Controllers/Follow/FollowController.php
@@ -16,9 +16,6 @@ use DB;
 
 class FollowController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
         // follower_code	The user who follows (that's you)
         // following_code	The user who is being followed
         public function index()
@@ -72,79 +69,23 @@ class FollowController extends Controller
 
             return response()->json($result);
         }
-
-        // public function index()
-        // {
-        //     $data = DB::select(' SELECT 
-        //             (SELECT getUserprofilepic(p.code)) AS profile_pic,
-        //             (SELECT getFullname(p.code)) AS fullname,
-        //             p.posts_uuid,
-        //             p.caption,
-        //             p.status,
-        //             p.created_at,
-        //             p.updated_at,
-        //             p.code AS post_owner
-        //         FROM posts AS p
-        //         LEFT JOIN follows AS f 
-        //             ON f.following_code = p.code AND f.follower_code = ? AND f.follow_status = "accepted"
-        //         WHERE p.status = 1
-        //         AND (f.follower_code IS NOT NULL OR p.code = ?)
-        //         ORDER BY p.created_at DESC
-        //     ', [Auth::user()->code, Auth::user()->code]);
-
-        //     $result = [];
-        //     for ($i = 0; $i < count($data); $i++) {
-
-        //         $attachements = DB::select('SELECT * FROM attachmentposts 
-        //             WHERE posts_uuid = ? AND (status = 1 OR ? = (SELECT code FROM posts WHERE posts_uuid = ?))', [
-        //             $data[$i]->posts_uuid, Auth::user()->code, $data[$i]->posts_uuid
-        //         ]);
-        //         $result[$i] = [
-        //             "profile_pic" => $data[$i]->profile_pic,
-        //             "Fullname" => $data[$i]->fullname,
-        //             "posts_uuid" => $data[$i]->posts_uuid,
-        //             "caption" => $data[$i]->caption,
-        //             "status" => $data[$i]->status,
-        //             "created_at" => $data[$i]->created_at,
-        //             "updated_at" => $data[$i]->updated_at,
-        //             "posts" => $attachements,
-        //         ];
-        //     }
-        //     return response()->json($result);
-        // }
-
-        
-
-    /**
-     * Show the form for creating a new resource.
-     */
+ 
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+ 
     public function store(Request $request)
     {
-        //
-
-
 
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
@@ -210,10 +151,6 @@ class FollowController extends Controller
         }
     }
     
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
