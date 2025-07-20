fac0c271a3abc86a31cbbb68676e1b26b728a722
Peds Yorpo <pedroyorpo17@gmail.com>
2025-07-20 09:24:23 +0800

    commit

--- app/Models/DAL/SearchHistoryDAL.php
+++ app/Models/DAL/SearchHistoryDAL.php
@@ -2,38 +2,27 @@
 
 namespace App\Models\DAL;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 
-class SearchHistoryDAL extends Model
+class SearchHistoryDAL
 {
-    use HasApiTokens, HasFactory, Notifiable;
-    protected $table = 'user_activity';
+    protected string $table = 'user_activity';
 
-    protected $fillable = [
-        'viewer_code',
-        'viewed_code',
-        'activity_type',
-        'timestamp',
-    ];
-
-    public $timestamps = false;
-
-
-    public function saveSearchHistory(array $data)
+    /**
+     * Save a new user activity using DB facade
+     *
+     * @param array $data
+     * @return bool
+     */
+    public function saveSearchHistory(array $data): bool
     {
-        return $self::create([
-            'viewer_code' => $data['viewer_code'],
-            'viewed_code' => $data['viewed_code'] ?? null,
+        return DB::table($this->table)->insert([
+            'viewer_code'   => $data['viewer_code'],
+            'viewed_code'   => $data['viewed_code'] ?? null,
             'activity_type' => $data['activity_type'],
-            'timestamp' => Carbon::now(),
+            'timestamp'     => Carbon::now(),
         ]);
     }
-
-
 }
