399cb89749eef343d2f439074dfae4cdde6f926c
Peds Yorpo <pedroyorpo17@gmail.com>
2025-07-22 22:45:20 +0800

    c

--- app/Models/DAL/ClientsDAL.php
+++ app/Models/DAL/ClientsDAL.php
@@ -447,8 +447,7 @@ class ClientsDAL extends Model
                     u.code,
                     u.is_online,
                     'history' AS source,
-                    f.id,
-                    COALESCE(f.follow_status, 'accepted') AS follow_status
+                    f.id
                 FROM users u
                 INNER JOIN resources r ON u.code = r.code
                 LEFT JOIN userprofiles up ON u.code = up.code
