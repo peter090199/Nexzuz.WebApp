345ad175961ec94e55983af20a4da67a2e026094
Peds Yorpo <pedroyorpo17@gmail.com>
2025-08-04 21:32:22 +0800

    commit

--- app/Http/Controllers/CV/UserSkills.php
+++ app/Http/Controllers/CV/UserSkills.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\CV;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\UsersUserskill;
+use App\Models\Userskill;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
