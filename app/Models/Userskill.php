7947da2ab6c82225807503fac4755b5d295b21f2
Peds Yorpo <pedroyorpo17@gmail.com>
2025-08-04 21:40:13 +0800

    commit

--- app/Models/Userskill.php
+++ app/Models/Userskill.php
@@ -1,21 +1,27 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\CV\DAL;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
-class Userskill extends Model
+
+
+class UserLanguages extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    
+    protected $table = 'userskills'; 
+
     protected $fillable = [
         'code',
         'transNo',
         'skills'
     ];
-
+ 
+   
 }
