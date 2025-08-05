b44429a8770c4af85133fdec153eb46516c6258a
Peds Yorpo <pedroyorpo17@gmail.com>
2025-08-05 21:35:20 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -178,6 +178,6 @@ Route::middleware(['auth:sanctum','checkstatus'])->group(function () {
     Route::delete('delete/{id}', [UserSkills::class, 'delete']);
     //seminar
     Route::post('saveSeminar', [UserSeminars::class, 'saveSeminar']);
-    Route::get('getSeminarByCode', [UserSkills::class, 'getSeminarByCode']);
+    Route::get('getSeminarByCode', [UserSeminars::class, 'getSeminarByCode']);
    
 });
