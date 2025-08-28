3a667638f6b94132621c188c3270a773806d7509
Peds Yorpo <pedroyorpo17@gmail.com>
2025-08-28 21:40:47 +0800

    commit

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -473,50 +473,27 @@ class ProfileController extends Controller
         }
     }
 
-    public function userAuthByCode(Request $request, $code)
-    {
-        $authUser = $request->user(); // must be logged in
-        if (!$authUser) {
+  
+    public function userAuthByCode($code) {
+        if (Auth::check()) {
+            $user = Resource::where('code', $code)->first();
+            if ($user) {
+                return response()->json([
+                    'success' => true,
+                    'message' => $user
+                ]);
+            }
             return response()->json([
                 'success' => false,
-                'message' => 'User is not authenticated'
-            ], 401);
-        }
-
-        $user = Resource::where('code', $code)->first();
-        if ($user) {
-            return response()->json([
-                'success' => true,
-                'message' => $user
+                'message' => 'User not found'
             ]);
         }
 
         return response()->json([
             'success' => false,
-            'message' => 'User not found'
-        ], 404);
+            'message' => 'User is not authenticated'
+        ]);
     }
-
-    // public function userAuthByCode($code) {
-    //     if (Auth::check()) {
-    //         $user = Resource::where('code', $code)->first();
-    //         if ($user) {
-    //             return response()->json([
-    //                 'success' => true,
-    //                 'message' => $user
-    //             ]);
-    //         }
-    //         return response()->json([
-    //             'success' => false,
-    //             'message' => 'User not found'
-    //         ]);
-    //     }
-
-    //     return response()->json([
-    //         'success' => false,
-    //         'message' => 'User is not authenticated'
-    //     ]);
-    // }
 
 
 
