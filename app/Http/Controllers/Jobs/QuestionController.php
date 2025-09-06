a64b879079108ed5b937a84511ab5cddc101ece7
Peds Yorpo <pedroyorpo17@gmail.com>
2025-09-06 11:15:01 +0800

    commit

--- app/Http/Controllers/Jobs/QuestionController.php
+++ app/Http/Controllers/Jobs/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Jobs;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Jobs\Question;
 
 class QuestionController extends Controller
 {
