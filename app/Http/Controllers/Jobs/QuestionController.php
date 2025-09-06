d0795b6a78ff976ac3e31e24a911076f01c4a616
Peds Yorpo <pedroyorpo17@gmail.com>
2025-09-06 11:18:12 +0800

    commit

--- app/Http/Controllers/Jobs/QuestionController.php
+++ app/Http/Controllers/Jobs/QuestionController.php
@@ -16,6 +16,7 @@ class QuestionController extends Controller
 
             // Validate the incoming request
             $validated = $request->validate([
+                'job_id' => 'required|integer',
                 'question_text' => 'required|string|max:255',
                 'job_name' => 'required|string',
             ]);
@@ -24,7 +25,7 @@ class QuestionController extends Controller
             // Save question to database
             $question = Question::create([
                 'question_text' => $validated['question_text'],
-                'job_id' =>  $validated['job_id'],
+                'job_id' => $validated['job_id'],
                 'job_name' => $validated['job_name'],
                 'role_code' => $user->role_code,
                 'code' => $user->code,
