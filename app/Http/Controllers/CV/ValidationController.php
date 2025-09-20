a7cf11785e9dc2c402879807b3def7859658ecd7
Peds Yorpo <pedroyorpo17@gmail.com>
2025-09-20 22:37:25 +0800

    commit

--- app/Http/Controllers/CV/ValidationController.php
+++ app/Http/Controllers/CV/ValidationController.php
@@ -7,20 +7,20 @@ use Illuminate\Http\JsonResponse;
 class ValidationController
 {
     /**
-     * Check for future dates in an array of items.
+     * Check if any date field in the array is in the future.
      *
      * @param array|null $items
-     * @param string $fieldName
+     * @param string $field
      * @return true|JsonResponse
      */
-    public static function futureDateCheck(?array $items, string $fieldName = 'date_completed')
+    public static function futureDateCheck(?array $items, string $field = 'date_completed')
     {
         if (empty($items) || !is_array($items)) {
-            return true; // nothing to check
+            return true; // nothing to validate
         }
 
         foreach ($items as $item) {
-            if (!empty($item[$fieldName]) && strtotime($item[$fieldName]) > strtotime(date('Y-m-d'))) {
+            if (isset($item[$field]) && strtotime($item[$field]) > strtotime(date('Y-m-d'))) {
                 return response()->json([
                     'success' => false,
                     'message' => 'The completion date cannot be in the future.',
@@ -28,6 +28,6 @@ class ValidationController
             }
         }
 
-        return true;
+        return true; // all dates are valid
     }
 }
