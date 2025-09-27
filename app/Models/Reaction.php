c1cc8b5425dc54e471fef73b48bfa69574a64cea
Peds Yorpo <pedroyorpo17@gmail.com>
2025-09-27 08:55:14 +0800

    commit

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,17 +10,14 @@ class Reaction extends Model
     use HasFactory;
 
     protected $table = 'reactionPost'; 
-    public $timestamps = false;
+
+    // Disable default timestamps since table doesn't have created_at / updated_at
+    public $timestamps = false;  
 
     protected $fillable = [
         'post_id',
         'post_uuidOrUind',
         'reaction',
-        'code'
-        // 'updated_at',
-        // 'create_at',
+        'code',
     ];
-
-} 
-
-
+}
