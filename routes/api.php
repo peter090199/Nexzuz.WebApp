b3bce57396a183a0f738b4a9e1d71effc27a8e1e
Peds Yorpo <pedroyorpo17@gmail.com>
2025-10-11 22:03:30 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -78,8 +78,12 @@
     Route::get('user', function (Request $request) {return $request->user();});
 
     //accessmenu
-    Route::Resource('accessmenu',AccessrolemenuController::class)->names('accessmenu');
-    Route::get('getModule', [AccessrolemenuController::class, 'getModule']);
+    // Route::Resource('accessmenu',AccessrolemenuController::class)->names('accessmenu');
+    // Route::get('getModule', [AccessrolemenuController::class, 'getModule']);
+    Route::middleware(['throttle:200,1'])->group(function () {
+        Route::get('getModule', [AccessrolemenuController::class, 'getModule']);
+        // Add other APIs here
+    });
 
     Route::post('logout',[LoginController::class,'logout'])->name('logout');
 
