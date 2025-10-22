d31a18e7f2ef7baf2ba7ff513e532a8ffabce375
Peds Yorpo <pedroyorpo17@gmail.com>
2025-10-22 21:46:22 +0800

    commit

--- app/Events/NewPostCreated.php
+++ app/Events/NewPostCreated.php
@@ -1,36 +1,29 @@
 <?php
 
 namespace App\Events;
-
+use App\Models\Post;
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-class NewPostCreated
+class NewPostCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public $post;
+
     public function __construct($post)
     {
         $this->post = $post;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
+    public function broadcastOn()
     {
-        return [
-            new PrivateChannel('posts'),
-        ];
+        return new Channel('posts');
     }
-    
+
     public function broadcastAs()
     {
         return 'NewPostCreated';
--- app/Events/NotificationCountUpdated.php
+++ app/Events/NotificationCountUpdated.php
@@ -1,42 +1,6 @@
 <?php
 namespace App\Events;
 
-// use Illuminate\Broadcasting\InteractsWithSockets;
-// use Illuminate\Broadcasting\Channel;
-// use Illuminate\Foundation\Events\Dispatchable;
-// use Illuminate\Queue\SerializesModels;
-
-
-// class NotificationCountUpdated
-// {
-//     use Dispatchable, InteractsWithSockets, SerializesModels;
-
-//     public $unreadCount;
-
-//     public function __construct($unreadCount)
-//     {
-//         $this->unreadCount = $unreadCount;
-//     }
-
-//     public function broadcastOn()
-//     {
-//         return new Channel('notification.count');
-//     }
-
-//     public function broadcastAs()
-//     {
-//         return 'notifications'; 
-//     }
-
-//     public function broadcastWith()
-//     {
-//         return [
-//             'unreadCount' => $this->unreadCount,
-//         ];
-//     }
-// }
-
-
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Support\Facades\Log;
