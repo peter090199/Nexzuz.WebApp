60bb0706b99b479267f19c595adb9e1d27ad31c8
Peds Yorpo <pedroyorpo17@gmail.com>
2025-11-23 15:58:08 +0800

    commit

--- app/Http/Controllers/Jobs/JobListController.php
+++ app/Http/Controllers/Jobs/JobListController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Jobs;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class JobListController extends Controller
 {
@@ -12,6 +13,22 @@ class JobListController extends Controller
     {
             $jobs = DB::table('jobPosting')
             ->where('recordstatus', 'active')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $jobs
+        ]);
+    }
+
+    public function getActiveJobsByCode()
+    {
+        $currentUserCode = Auth::user()->code;
+
+        $jobs = DB::table('jobPosting')  
+            ->where('recordstatus', 'active')
+            ->where('code', $currentUserCode)  // FILTER BY OWNER
             ->orderBy('created_at', 'desc')
             ->get();
 
--- routes/api.php
+++ routes/api.php
@@ -220,6 +220,7 @@
     Route::delete('deleteJobPosting/{id}', [JobPostingController::class, 'deleteJobPosting']);
     //JobList
     Route::get('getActiveJobs', [JobListController::class, 'getActiveJobs']);
+    Route::get('getActiveJobsByCode', [JobListController::class, 'getActiveJobsByCode']);
     //company profile
     Route::get('company/profile/{code}', [ProfileController::class, 'userAuthByCode']);
     Route::get('country_codes', function () {
